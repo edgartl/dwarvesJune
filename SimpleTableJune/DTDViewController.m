54034e279d5f68bc693b20d065ca342b911c8186
Tom Edgar <edgartlMBPr@Toms-MacBook-Pro.local>
2014-06-25 10:36:02 -0500

    Phase 3 - DidSelectRow has been implemented and result shown in UIAlertView.

--- SimpleTableJune/DTDViewController.m
+++ SimpleTableJune/DTDViewController.m
@@ -50,11 +50,33 @@
     UIImage *imageHighlight = [UIImage imageNamed:@"starHigh"];
     cell.imageView.highlightedImage = imageHighlight;
     
+//    if (indexPath.row < 7) {
+//        cell.detailTextLabel.text = @"Mr. Disney";
+//    } else {
+//        cell.detailTextLabel.text = @"Mr. Tolkien";
+//    }
+    
     
     cell.textLabel.text = self.dwarves[indexPath.row];
     //NSLog(@"Called");
     return cell;
 }
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView
+didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *message = [NSString stringWithFormat:@"You Selected %@", self.dwarves[indexPath.row]];
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Row Selected"
+                                                    message:message
+                                                   delegate:nil
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
+}
+
 
 @end
 
